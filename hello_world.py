9eca754b0e60f905372efc37aa6f4748793cb7cd
Your Name <you@example.com>
2019-07-01 16:36:48 -0500

    first commit

---
+++ hello_world.py
@@ -0,0 +1,29 @@
+print("Hello World")
+
+name = "Stvew"
+print("Hello" , name)
+
+name = "Stvew"
+print("Hello " + name)
+
+num = 6
+print("Hello" , num)
+
+num = '6'
+print("Hello" + num)
+
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("I love to eat {} and {}.".format(fave_food1 , fave_food2))
+
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print(f"I love to eat {fave_food1} and {fave_food2}.")
+
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print(f"I love to eat {fave_food1} and {fave_food2}.")
+
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("I love to eat %s and %s." % (fave_food1, fave_food2))
